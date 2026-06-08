ac04d43dadf89f32197aa5ed8bff61b3f1c76dda
Khushi Sahu <khushii77e@gmail.com>
2026-06-08 11:13:31 +0530

    Complete Library Interface assignment implementation

---
+++ Week_1/OOPS/LibraryInterfaceDemo.java
@@ -0,0 +1,39 @@
+// Main class to test interface implementation
+
+public class LibraryInterfaceDemo {
+    public static void main(String[] args) {
+
+        System.out.println("----- Kid User Test Cases -----");
+
+        KidUsers kid = new KidUsers();
+
+        kid.age = 10;
+        kid.registerAccount();
+
+        kid.age = 18;
+        kid.registerAccount();
+
+        kid.bookType = "Kids";
+        kid.requestBook();
+
+        kid.bookType = "Fiction";
+        kid.requestBook();
+
+
+        System.out.println("\n----- Adult User Test Cases -----");
+
+        AdultUser adult = new AdultUser();
+
+        adult.age = 5;
+        adult.registerAccount();
+
+        adult.age = 23;
+        adult.registerAccount();
+
+        adult.bookType = "Kids";
+        adult.requestBook();
+
+        adult.bookType = "Fiction";
+        adult.requestBook();
+    }
+}
