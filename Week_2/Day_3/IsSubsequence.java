bd6591cef1e36a9c3987a1b4df0cdaf8d59dd07f
Khushi Sahu <khushii77e@gmail.com>
2026-06-10 20:56:15 +0530

    Add Is Subsequence solution

---
+++ Week_2/Day_3/IsSubsequence.java
@@ -0,0 +1,18 @@
+// Problem: Check whether string s is a subsequence of string t.
+// Approach: we use two pointers and move forward when characters match.
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j))i++;
+
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
