53f9db904d64462437cb0d254ddd13c2e5faec51
Khushi Sahu <khushii77e@gmail.com>
2026-06-17 23:17:14 +0530

    Update Valid Parentheses solution

--- Week_3/Day3/ValidParentheses.java
+++ Week_3/Day3/ValidParentheses.java
@@ -2,3 +2,33 @@
 // Approach: we will use a stack to match opening and closing brackets.
 // Time Complexity: O(n)
 // Space Complexity: O(n)
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+
+                if (st.isEmpty()) return false;
+
+                char ch2 = st.peek();
+
+                if ((ch2 == '(' && ch == ')') ||
+                    (ch2 == '{' && ch == '}') ||
+                    (ch2 == '[' && ch == ']'))
+                    st.pop();
+
+                else
+                    return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
