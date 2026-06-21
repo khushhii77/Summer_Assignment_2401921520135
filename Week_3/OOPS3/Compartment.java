21566f7eb8612de4ccdf53efb84c728f2c43a6b5
Khushi Sahu <khushii77e@gmail.com>
2026-06-21 18:26:43 +0530

    Add Compartment abstract class

---
+++ Week_3/OOPS3/Compartment.java
@@ -0,0 +1,6 @@
+// Abstract class representing a railway compartment.
+// Contains abstract notice() method to be overridden by child classes.
+
+abstract class Compartment {
+    public abstract String notice();
+}
