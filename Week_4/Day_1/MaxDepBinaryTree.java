8e2eb54f3951c2cf610f40a9158db405826c8af4
Khushi Sahu <khushii77e@gmail.com>
2026-06-22 21:25:09 +0530

    Add Maximum Depth of Binary Tree solution

---
+++ Week_4/Day_1/MaxDepBinaryTree.java
@@ -0,0 +1,17 @@
+// Problem: Find the maximum depth of a binary tree.
+// Approach: for this, we use recursion to calculate depth of left and right subtrees.
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        int ans = Math.max(left, right) + 1;
+
+        return ans;
+    }
+}
