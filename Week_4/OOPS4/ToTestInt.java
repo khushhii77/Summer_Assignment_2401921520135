88b87baebdd1103849b239caa33a136734afefb2
Khushi Sahu <khushii77e@gmail.com>
2026-06-26 16:41:34 +0530

    Add ToTestInt class

---
+++ Week_4/OOPS4/ToTestInt.java
@@ -0,0 +1,12 @@
+// Creates Arithmetic object.
+// Calls square() method.
+
+public class ToTestInt {
+
+    public static void main(String[] args) {
+        Arithmetic obj = new Arithmetic();
+      
+        System.out.println(obj.square(5));
+    }
+  
+}
