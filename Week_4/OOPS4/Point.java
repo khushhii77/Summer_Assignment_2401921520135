5454faafbecbd5b0b1c52da1508f80fd4d0ce28f
Khushi Sahu <khushii77e@gmail.com>
2026-06-26 16:44:49 +0530

    Add Point class

---
+++ Week_4/OOPS4/Point.java
@@ -0,0 +1,34 @@
+// Point class with constructors.
+// Contains setter methods.
+
+class Point {
+    private int x, y;
+
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void setX(int x) {
+        this.x = x;
+    }
+
+    void setY(int y) {
+        this.y = y;
+    }
+
+    void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void display() {
+        System.out.println(x + " " + y);
+    }
+  
+}
