277694c19a15351fb27a517ca9b2d6f0745a5228
Khushi Sahu <khushii77e@gmail.com>
2026-06-27 09:56:53 +0530

    Add PointDemo class

---
+++ Week_4/OOPS4/PointDemo.java
@@ -0,0 +1,12 @@
+// Tests Point class methods.
+
+public class PointDemo {
+
+    public static void main(String[] args) {
+        Point p = new Point();
+
+        p.setXY(10, 20);
+        p.display();
+    }
+  
+}
